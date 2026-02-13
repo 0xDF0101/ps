2b0b4554645ea7ab3e0b38aa3845a36215025925
Eugene <0xdf0101@gmail.com>
2026-02-13 10:04:05 +0900

    [Bronze V] Title: A+B - 4, Time: 88 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,16 +1,23 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextInt()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a + b);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        while((line = bufferedReader.readLine())!=null) {
+
+            String[] nums = line.split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            System.out.println(a+b);
+
         }
+
 
 
     }
